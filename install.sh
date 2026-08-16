6bdb3705b80d68fe54387fa1734ceedfd6e36579
Florian <florian.thamm03+github.com>
2026-08-16 13:37:31 +0200

    sh test

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# @nav: functions
+is_installed() {
+    if dpkg -s "$1" &> /dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+is_found() {
+    if [[ -f "$1" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# do_install_list "${<var>[*]}"
+do_install_list() {
+  sudo pacman -S --needed $1
+}
+
+do_install_cargo_list() {
+  sudo cargo -S --needed $1
+}
+
+copy_file() {
+  src=$1
+  dest=$2
+  name=$3
+  
+  sudo mkdir -p $dest
+  if is_found() "$2$3"; then
+    sudo mv "$2$3" "$2$3.old"
+  fi
+
+  sudo cp "$1" "$2$3"
+}
+
+# @nav: packages
+# package lists
+drivers=("amd-ucode" "nvidia-open" "nvidia-utils" "lib32-nvidia-utils")
+packages=("base" "base-devel" "networkmanager" "git" "curl" "openssh" "zip" "unzip" "zsh" "kitty" "firefox" "noto-fonts" "otf-firamono-nerd" "neovim" "thunar" "thunar-archive-plugin" "thunar-media-tags-plugin" "thunar-volman" "xarchiver" "hyprland" "hyprpaper" "hyprshot" "noctalia" "greetd" "greetd-tuigreet" "lsd" "nodejs" "rust" "npm" "fastfetch" "steam" "discord")
+cargo=("noctalia-greeter" "pokeget")
+
+# package install
+do_install_list "${drivers[*]} ${packages[*]}"
