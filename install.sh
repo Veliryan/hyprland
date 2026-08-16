a2a4b431bbd641ddbf2aae88f6100bb5091ae172
Florian <florian.thamm03+github.com>
2026-08-16 14:06:39 +0200

    google chrome

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ copy_file() {
 # package lists
 drivers=("amd-ucode" "nvidia-open" "nvidia-utils" "lib32-nvidia-utils")
 packages=("base" "base-devel" "networkmanager" "git" "curl" "openssh" "zip" "unzip" "zsh" "kitty" "firefox" "noto-fonts" "otf-firamono-nerd" "neovim" "thunar" "thunar-archive-plugin" "thunar-media-tags-plugin" "thunar-volman" "xarchiver" "hyprland" "hyprpaper" "hyprshot" "noctalia" "greetd" "greetd-tuigreet" "lsd" "nodejs" "rust" "npm" "fastfetch" "steam" "discord")
-paru=("noctalia-greeter" "pokeget")
+paru=("noctalia-greeter" "pokeget" "google-chrome")
 
 # package install pacman
 do_install_list "${drivers[*]} ${packages[*]}"
