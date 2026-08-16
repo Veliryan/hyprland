56cad5e13b8a893193302329e6422d0cc06448df
Florian <florian.thamm03+github@proton.me>
2026-08-16 22:19:01 +0200

    Add hdparm to package list in install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ copy_file() {
 # @nav: packages
 # package lists
 drivers=("amd-ucode" "nvidia-open" "nvidia-utils" "lib32-nvidia-utils")
-packages=("base" "base-devel" "networkmanager" "git" "curl" "openssh" "zip" "unzip" "zsh" "kitty" "firefox" "noto-fonts" "otf-firamono-nerd" "neovim" "thunar" "thunar-archive-plugin" "thunar-media-tags-plugin" "thunar-volman" "xarchiver" "hyprland" "hyprpaper" "hyprshot" "noctalia" "greetd" "lsd" "nodejs" "rust" "npm" "fastfetch" "steam" "discord")
+packages=("base" "base-devel" "networkmanager" "git" "curl" "openssh" "zip" "unzip" "zsh" "kitty" "firefox" "noto-fonts" "otf-firamono-nerd" "neovim" "thunar" "thunar-archive-plugin" "thunar-media-tags-plugin" "thunar-volman" "xarchiver" "hyprland" "hyprpaper" "hyprshot" "noctalia" "greetd" "lsd" "nodejs" "rust" "npm" "fastfetch" "hdparm" "steam" "discord")
 paru=("noctalia-greeter" "pokeget" "google-chrome" "mpv" "mpvpaper")
 neovim_dep=("neovim" "git" "ripgrep" "fd" "tar" "curl" "tree-sitter" "gcc" "npm" "zip" "unzip")
 
