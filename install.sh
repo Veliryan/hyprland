8b3a1b86b8eee81d63dc230076078b3de729fc84
Florian <florian.thamm03+github@proton.me>
2026-08-17 07:28:09 +0200

    Update package list in install.sh
    Added 'gvfs' and 'gnome-disk-utility' to the packages list.

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ copy_file() {
 # @nav: packages
 # package lists
 drivers=("amd-ucode" "nvidia-open" "nvidia-utils" "lib32-nvidia-utils")
-packages=("base" "base-devel" "networkmanager" "git" "curl" "openssh" "zip" "unzip" "zsh" "kitty" "firefox" "noto-fonts" "otf-firamono-nerd" "neovim" "thunar" "thunar-archive-plugin" "thunar-media-tags-plugin" "thunar-volman" "xarchiver" "hyprland" "hyprpaper" "hyprshot" "noctalia" "greetd" "lsd" "nodejs" "rust" "npm" "fastfetch" "hdparm" "steam" "discord" "udisks2" "polkit")
+packages=("base" "base-devel" "networkmanager" "git" "curl" "openssh" "zip" "unzip" "zsh" "kitty" "firefox" "noto-fonts" "otf-firamono-nerd" "neovim" "thunar" "thunar-archive-plugin" "thunar-media-tags-plugin" "thunar-volman" "xarchiver" "hyprland" "hyprpaper" "hyprshot" "noctalia" "greetd" "lsd" "nodejs" "rust" "npm" "fastfetch" "hdparm" "steam" "discord" "udisks2" "polkit" "gvfs" "gnome-disk-utility" "polkit-gnome")
 paru=("noctalia-greeter" "pokeget" "google-chrome" "mpv" "mpvpaper")
 neovim_dep=("neovim" "git" "ripgrep" "fd" "tar" "curl" "tree-sitter" "tree-sitter-cli" "gcc" "npm" "zip" "unzip")
 
